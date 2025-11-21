3453a99ba8bbdc4a25925703323535ca71e67416
Zhihao Li <lizhihaoms@gmail.com>
2025-11-21 06:46:00 +0000

    update to the latest training version

--- scripts/talker_train.sh
+++ scripts/talker_train.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # custom config
-DATA=./data
 TRAINER=FlowMatchingTrainer  # DiffPoseTalkTrainer or FlowMatchingTrainer 
 
 DATASET=HDTF_TFHP
@@ -9,12 +8,15 @@ TAG=Train
 
 DIR=output/${DATASET}/${TRAINER}/${TAG}
 STYLEDIR=output/${DATASET}/StyleEncoderTrainer/${TAG}/model/model.pth.tar-iter-100000
-mkdir -p ${DIR}
-python -m main.train \
-  --config-file config/flowmatching_trainer_config.yaml \
-  ENV.OUTPUT_DIR ${DIR} \
-  DATASET.ROOT ${DATA} \
-  DATASET.NAME ${DATASET} \
-  TRAINER.NAME ${TRAINER} \
-  ENV.EXTRA.STYLE_ENC_CKPT ${STYLEDIR} \
-  2>&1 | tee ${DIR}/output.log
+if [ -d "$DIR" ]; then
+  echo "Results are available in ${DIR}. Skip this job"
+  rm -rf ${DIR}
+else
+  /zhli/miniconda3/envs/diffposetalk/bin/python -m main.train \
+    --config-file config/flowmatching_trainer_config.yaml \
+    ENV.OUTPUT_DIR ${DIR} \
+    DATASET.NAME ${DATASET} \
+    TRAINER.NAME ${TRAINER} \
+    ENV.EXTRA.STYLE_ENC_CKPT ${STYLEDIR} \
+    2>&1 | tee ${DIR}/output.log
+fi
